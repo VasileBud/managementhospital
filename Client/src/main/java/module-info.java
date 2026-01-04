70075fab97e74394177d061ac400bae5d7f9655e
Vasile Bud <vasilebud9@gmail.com>
2026-01-04 11:38:24 +0200

    first commit

---
+++ Client/src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.hospital_management.client {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.hospital_management.client to javafx.fxml;
+    exports com.hospital_management.client;
+}
